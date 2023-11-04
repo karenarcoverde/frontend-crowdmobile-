eae174b0759bc4c95e60cede2a9eb55b4ff126b1
Karen dos Anjos Arcoverde <karenarcoverde@poli.ufrj.br>
2023-11-04 13:51:23 -0300

    done tabs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import './App.css';
 import Sidebar from './components/Sidebar';
 import styled from "styled-components";
+import { Tabs, Tab } from 'react-bootstrap';
 
 const Container = styled.div`
   display: flex;
@@ -32,6 +33,61 @@ const RightDiv = styled.div`
   }
 `;
 
+const StyledTabs = styled(Tabs)`
+  // Set the border for the tabs container
+  border: 1px solid #ddd;
+  color: #333;
+  margin: 20px;
+  background-color: #f5f5f5;
+  padding: 5px 5px 0 5px;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+  border-bottom-color: #fff;
+
+  // Override the default width of nav-tabs
+  .nav {
+    display: flex; // This will make tabs take equal space
+    margin-bottom: 0; // Remove if any margin is applied
+    width: 100%; // Set width to fill the parent
+    border-color: #fff;
+  }
+
+  .nav-item {
+    flex: 1; // This will make each tab take equal space
+    text-align: center; // This will center the tab titles
+
+    .nav-link {
+      display: block; // Make the link fill the .nav-item container
+      width: 100%; // Ensure it fills the width
+      height: 100%; // Ensure it fills the height (if necessary)
+      padding: 0.5rem 1rem; // Adjust padding as needed
+
+      // Default state
+      color: #777;
+
+      // Hover state
+      &:hover {
+        color: #777;
+        background-color: #e9e9e9;
+      }
+
+      // Active state
+      &.active {
+        color: #333;
+        background-color: #fff;
+        border-color: #ccc;
+        border-bottom-color: transparent;
+      }
+    }
+  }
+
+  // If you need to style the bottom border of the active tab to match
+  &.tab-pane {
+    border-bottom: 1px solid #ddd;
+  }
+`;
+
+
 const baseURL = `http://127.0.0.1:5000`;
 function App() {
   const [columns, setColumns] = useState([]);
@@ -51,7 +107,14 @@ function App() {
   return (
     <Container>
       <LeftDiv>
-        {/* LeftDiv content goes here */}
+        <StyledTabs defaultActiveKey="filters" id="uncontrolled-tab-example">
+          <Tab eventKey="filters" title="Filters">
+            {/* Tab content */}
+          </Tab>
+          <Tab eventKey="sql" title="SQL">
+            {/* Tab content */}
+          </Tab>
+        </StyledTabs>
       </LeftDiv>
       <RightDiv>
         <Sidebar data={columns} />
