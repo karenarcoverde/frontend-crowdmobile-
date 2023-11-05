49592b3dddd3dbb0fbdf14fc8f14643045cee32f
Karen dos Anjos Arcoverde <karenarcoverde@poli.ufrj.br>
2023-11-04 21:18:41 -0300

    formated tabsql

--- src/components/TabSQL.jsx
+++ src/components/TabSQL.jsx
@@ -35,56 +35,56 @@ const InfoIconWrapper = styled.div`
 `;
 
 function TabSQL() {
-  const [sqlQuery, setSqlQuery] = useState('');
-  const [showModal, setShowModal] = useState(false);
+    const [sqlQuery, setSqlQuery] = useState('');
+    const [showModal, setShowModal] = useState(false);
 
-  const handleInputChange = (e) => {
-    setSqlQuery(e.target.value);
-  };
+    const handleInputChange = (e) => {
+        setSqlQuery(e.target.value);
+    };
 
-  const handleQuerySubmit = () => {
-    console.log(sqlQuery);
-  };
+    const handleQuerySubmit = () => {
+        console.log(sqlQuery);
+    };
 
-  const handleCloseModal = () => setShowModal(false);
-  const handleShowModal = () => setShowModal(true);
+    const handleCloseModal = () => setShowModal(false);
+    const handleShowModal = () => setShowModal(true);
 
-  return (
-    <SqlEditorContainer>
-      <SqlInputWrapper>
-        <StyledFormControl
-          as="textarea"
-          rows={10}
-          placeholder={"SELECT \"CLIENT_LATITUDE\", \"CLIENT_LONGITUDE\", \"LATENCY\" FROM android_extracts_all"}
-          value={sqlQuery}
-          onChange={handleInputChange}
-        />
-        <InfoIconWrapper>
-          <InfoCircle size={20} onClick={handleShowModal} />
-        </InfoIconWrapper>
-      </SqlInputWrapper>
+    return (
+        <SqlEditorContainer>
+            <SqlInputWrapper>
+                <StyledFormControl
+                    as="textarea"
+                    rows={10}
+                    placeholder={"SELECT \"CLIENT_LATITUDE\", \"CLIENT_LONGITUDE\", \"LATENCY\" FROM android_extracts_all"}
+                    value={sqlQuery}
+                    onChange={handleInputChange}
+                />
+                <InfoIconWrapper>
+                    <InfoCircle size={20} onClick={handleShowModal} />
+                </InfoIconWrapper>
+            </SqlInputWrapper>
 
-      <ActionWrapper>
-        <Button variant="primary" onClick={handleQuerySubmit}>
-          Run Query
-        </Button>
-      </ActionWrapper>
+            <ActionWrapper>
+                <Button variant="primary" onClick={handleQuerySubmit}>
+                    Run Query
+                </Button>
+            </ActionWrapper>
 
-      <Modal show={showModal} onHide={handleCloseModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>How to use SQL Tab</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          Some interesting info about SQL queries can go here.
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseModal}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </SqlEditorContainer>
-  );
+            <Modal show={showModal} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>How to use SQL Tab</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Some interesting info about SQL queries can go here.
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseModal}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </SqlEditorContainer>
+    );
 }
 
 export default TabSQL;
