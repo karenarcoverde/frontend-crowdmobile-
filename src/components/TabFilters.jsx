f3b705b9a7cd4f3049f94bbd7b799af7d56c58a7
Karen dos Anjos Arcoverde <karenarcoverde@poli.ufrj.br>
2023-12-23 23:29:01 -0300

    added date range

--- src/components/TabFilters.jsx
+++ src/components/TabFilters.jsx
@@ -6,6 +6,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const TabFilters = () => {
     const [filters, setFilters] = useState([]);
     const [filterValues, setFilterValues] = useState({});
+    const [dateTimeRange, setDateTimeRange] = useState({
+        startDate: '',
+        endDate: '',
+    });
 
     useEffect(() => {
         axios.get('http://127.0.0.1:5000/get_filters')
@@ -33,22 +37,52 @@ const TabFilters = () => {
         });
     };
 
+    const handleDateTimeChange = (event, dateType) => {
+        const { value } = event.target;
+        setDateTimeRange({
+            ...dateTimeRange,
+            [dateType]: value
+        });
+    };
+
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log('Filters submitted:', filterValues);
+        console.log('Date and Time Range:', dateTimeRange.startDate, dateTimeRange.endDate);
     };
 
     return (
         <Form onSubmit={handleSubmit} style={{ display: 'flex', flexWrap: 'wrap', alignItems: 'flex-end', marginLeft: '15px' }}>
+            <div style={{ display: 'flex', alignItems: 'center', margin: '5px' }}>
+                <Form.Group controlId="formStartDate">
+                    <Form.Label>Start Date and Time</Form.Label>
+                    <Form.Control
+                        type="datetime-local"
+                        value={dateTimeRange.startDate}
+                        onChange={(e) => handleDateTimeChange(e, 'startDate')}
+                    />
+
+                </Form.Group>
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center', margin: '5px' }}>
+                <Form.Group controlId="formEndDate">
+                    <Form.Label>End Date and Time</Form.Label>
+                    <Form.Control
+                        type="datetime-local"
+                        value={dateTimeRange.endDate}
+                        onChange={(e) => handleDateTimeChange(e, 'endDate')}
+                    />
+                </Form.Group>
+            </div>
             {filters.map((filter, index) => (
                 <div style={{ display: 'flex', alignItems: 'center', margin: '5px' }} key={index}>
                     <Form.Group>
                         <Form.Label>{filter.name}</Form.Label>
-                        <Form.Select 
+                        <Form.Select
                             value={filterValues[filter.name]}
                             onChange={(e) => handleSelectChange(e, filter.name)}
                         >
-                            <option value=""> </option> {/* Opção de string vazia */}
+                            <option value=""> </option>
                             {filter.options.map((option, optionIndex) => (
                                 <option key={optionIndex} value={option}>{option}</option>
                             ))}
